5b3d03ee845550ee1a67dedb7e1f6e9280b4540f
Roshan Pandey <pandeyroshan556@gmail.com>
2019-08-03 13:04:26 +0530

    Update forms.py

--- entries/forms.py
+++ entries/forms.py
@@ -8,6 +8,6 @@ class EntryForm(ModelForm):
         fields = ('title','author','text',)
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
-        self.fields['title'].widget.attrs.update({'class': 'textarea', 'placeholder': 'Short Title'})
-        self.fields['author'].widget.attrs.update({'class': 'textarea', 'placeholder': 'Author Name'})
-        self.fields['text'].widget.attrs.update({'class': 'textarea', 'placeholder': 'Describe Todays Work'})
+        self.fields['title'].widget.attrs.update({'class': 'textarea', 'placeholder': "Short Title of today's work"})
+        self.fields['author'].widget.attrs.update({'class': 'textarea', 'placeholder': " "})
+        self.fields['text'].widget.attrs.update({'class': 'textarea', 'placeholder': "Describe Today's Work"})
