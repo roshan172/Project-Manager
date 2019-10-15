ab61be28d8c3860523b409c5ef47fa94f47e2975
Roshan Pandey <pandeyroshan556@gmail.com>
2019-10-15 10:00:00 +0100

    Update settings.py

--- entries/models.py
+++ entries/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from model_utils import Choices
+from model_utils import choices
+from django.utils import timezone
 
 
 # Create your models here.
@@ -10,7 +11,7 @@ class Entry(models.Model):
     authors = [(ROSHAN, 'Roshan Pandey'),(BHARAT, 'Bharat Hasija')]
     author = models.CharField(max_length=20,choices=authors,default=ROSHAN)
     text= models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
         return self.title
